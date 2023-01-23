714d05c82cc9a3152446798431b04fb47ff4b6bb
Marcos <marcos.quinteros2003@gmail.com>
2023-01-23 12:07:57 -0300

    Initial commit

---
+++ comentarios.py
@@ -0,0 +1,15 @@
+#Variables que indican la informacion de una persona
+nombre = "Marcos"
+apellido = "Quinteros"
+edad = 19
+
+'''
+Cree un programa que pide el nombre
+apellido, 
+edad,
+profesion y despues imprima los resultados
+'''
+
+profesion = "ing"
+
+print(f"tu nombre es: {nombre}, tu apellido es: {apellido}, tu edad es: {edad} y tu profesion es: {profesion}")
---
+++ variables.py
@@ -0,0 +1,15 @@
+nombre = "Marcos"
+numero = 12345
+estatura = 1.72
+
+print("Hola " + nombre + " bienvenido al mundo de la programacion con Python")
+
+print("Hola " + nombre + " tu numero es: " + str(numero))
+
+print(f'Hola {nombre} tu numero telefonico es: {numero}')
+
+print(f'Hola {nombre} + tu numero telefonico es: {numero} + y tu estatura es: {estatura}')
+
+print(type(nombre))
+print(type(numero))
+print(type(estatura))

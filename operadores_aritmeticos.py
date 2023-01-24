7bd6302d459cebf7624e569a6263222e61418fe5
Marcos <marcos.quinteros2003@gmail.com>
2023-01-24 10:58:27 -0300

    operadores

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,20 @@
+#Pedir numeros al usuario
+numero1 = int(input("Ingresa un número: "))
+numero2 = int(input("Ingresa un número: "))
+
+#Imprimir resultados
+
+#Suma
+print(f'La suma es: {numero1 + numero2}')
+
+#Resta
+print(f'La suma es: {numero1 - numero2}')
+
+#Multiplicacion
+print(f'La suma es: {numero1 * numero2}')
+
+#Division
+print(f'La suma es: {numero1 / numero2}')
+
+#Modulo
+print(f'La suma es: {numero1 % numero2}')
---
+++ operadores_logicos.py
@@ -0,0 +1,8 @@
+a = True
+b = False
+
+print(a and b)
+
+print(a or b)
+
+print(not a)
